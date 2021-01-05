77ca48ba5bf244d59b5547ad22059a2ac9828e4e
Thomas Castleman <thomas7rex@gmail.com>
2021-01-05 16:40:08 -0500

    change the implementation of lex to return a vector of tokens instead of a stream

--- src/lex.rs
+++ src/lex.rs
@@ -1,157 +1,136 @@
-use std::iter::Iterator;
 use regex::Regex;
-
-/*
-  The lexer generalizes over the lexical patterns, and what tokens 
-  look like. 
-*/
 
 // ---------------------------------------------------------------------------
 /// TEMP: tokens outputted by the lexer
 /// ultimately this should be generic
 #[derive(Debug)]
 pub struct Token<'a> {
-	name: &'a str,
-	lexeme: &'a str
+    name: &'a str,
+    lexeme: &'a str,
 }
 
 impl<'a> Token<'a> {
-	pub fn new(name: &'a str, lexeme: &'a str) -> Token<'a> {
-		Token { name: name, lexeme: lexeme }
-	}
+    pub fn new(name: &'a str, lexeme: &'a str) -> Token<'a> {
+        Token {
+            name: name,
+            lexeme: lexeme,
+        }
+    }
 }
 // ---------------------------------------------------------------------------
 
 /// Error type for lexer errors.
-/// failing_text is the rest of the text at the point
-/// at which the invalid token was encountered
+/// - NoPatterns: tried to lex with no patterns added.
+/// - InvalidToken(idx): token that doesn't match any pattern
+/// 	found at index idx in the input string.
 #[derive(Debug)]
-pub struct LexError<'a> {
-	failing_text: &'a str
+pub enum LexerError {
+    NoPatterns,
+    InvalidToken(usize),
 }
 
-impl<'a> LexError<'a> {
-	fn new(text: &str) -> LexError {
-		LexError { failing_text: text }
-	}
-}
-
-use std::fmt;
-impl<'a> fmt::Display for LexError<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "invalid token encountered at `{}`", self.failing_text)
+// implement Display so LexerErrors can be printed
+impl std::fmt::Display for LexerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            LexerError::NoPatterns => {
+                write!(f, "lexer error: no patterns supplied to lexer")
+            }
+            LexerError::InvalidToken(idx) => {
+                write!(f, "lexer error: invalid token at index {}", idx)
+            }
+        }
     }
 }
 
-/// The Lexer itself
+/// The Lexer itself.
+/// - spec: a vector of (pattern, make_token) tuples, where
+/// 	pattern is a regular expr, and make_token is a function
+/// 	which constructs a token from a match of the pattern.
 pub struct Lexer<'a> {
-	// where F: Fn(&str) -> Token {
-	spec: Vec<(&'a Regex, fn(&str) -> Token)>
+    spec: Vec<(&'a Regex, fn(&str) -> Token)>,
 }
 
 impl<'a> Lexer<'a> {
-	/// Constructs a new Lexer instance with an empty spec
-	pub fn new() -> Lexer<'a> {
-		Lexer { spec: Vec::new() }
-	}
+    /// Constructs a new Lexer instance with an empty spec
+    pub fn new() -> Lexer<'a> {
+        Lexer { spec: Vec::new() }
+    }
 
-	/// Adds a lexical rule to the lexer, which describes a given token's 
-	/// structure (regular expr) and how to build a token out of it.
-	pub fn add_rule(&mut self, pattern: &'a Regex, make_token: fn(&str) -> Token) {
-		self.spec.push((pattern, make_token));
-	}
+    /// Adds a lexical rule to the lexer, which describes a given token's
+    /// structure (regular expr) and how to build a token out of it.
+    pub fn add_pattern(
+        &mut self,
+        pattern: &'a Regex,
+        make_token: fn(&str) -> Token,
+    ) {
+        self.spec.push((pattern, make_token));
+    }
 
-	/// Produces a stream of tokens by runnig this lexer on given text
-	pub fn lex(&self, text: &'a str) -> TokenStream {
-		TokenStream::new(&self, text)
-	}
+    /// Produces a vector of tokens lexed from the given text, or a lexer error.
+    pub fn lex(&self, text: &'a str) -> Result<Vec<Token>, LexerError> {
+        // check that lexical patterns exist in internal state
+        if self.spec.is_empty() {
+            Err(LexerError::NoPatterns)
+        } else {
+            let mut tokens = Vec::new();
 
-	/// Parses a single token off the beginning of the given string,
-	/// returning the token and the rest of the string, or None
-	/// if there is no more input.
-	pub fn get_next_token(&self, text: &'a str) 
-		-> Option<Result<(Token, &'a str), LexError>> {
-		// indicate end of input if encountered
-		if text.is_empty() {
-			None
-		} else {
-			let mut longest_match: Option<&str> = None;
-			let mut longest_token: Option<Token> = None;
+            // rest begins as entire text, starting index 0
+            let mut rest = &text[..];
+            let mut rest_start_idx = 0;
 
-			// for each pattern in the lexical spec
-			for (pat, mk_tok) in self.spec.iter() {
-				// test pattern against text
-				match pat.find(text) {
-					Some(mat) => {
-						// ignore match if it doesn't cover the text beginning
-						if mat.start() != 0 {
-							continue;
-						} else {
-							let lexeme = &text[..mat.end()];
-							let token = mk_tok(lexeme);
+            // while there is text left to tokenize
+            while !rest.is_empty() {
+                let mut longest_match: Option<&str> = None;
+                let mut longest_token: Option<Token> = None;
 
-							// update longest match if new max found
-							match longest_match {
-								None => {
-									longest_match = Some(lexeme);
-									longest_token = Some(token);
-								},
-								Some(long_lexeme) => {
-									if lexeme.len() > long_lexeme.len() {
-										longest_match = Some(lexeme);
-										longest_token = Some(token);
-									}
-								}
-							}
+                // for each pattern in the lexical spec
+                for (pat, mk_tok) in self.spec.iter() {
+                    // test pattern against text
+                    match pat.find(rest) {
+                        Some(mat) => {
+                            // ignore match if it doesn't start from beginning
+                            if mat.start() != 0 {
+                                continue;
+                            } else {
+                                let lexeme = &rest[..mat.end()];
+                                let token = mk_tok(lexeme);
 
-						}
-					}
-					None => (),
-				}
-			}
+                                // update longest match if new max found
+                                match longest_match {
+                                    None => {
+                                        longest_match = Some(lexeme);
+                                        longest_token = Some(token);
+                                    }
+                                    Some(long_lexeme) => {
+                                        if lexeme.len() > long_lexeme.len() {
+                                            longest_match = Some(lexeme);
+                                            longest_token = Some(token);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        None => (),
+                    }
+                }
 
-			match (longest_token, longest_match) {
-				(Some(tok), Some(lexeme)) => {
-					let rest_of_text = &text[lexeme.len()..];
-					Some(Ok((tok, rest_of_text)))
-				},
-				_ => Some(Err(LexError::new(text))),
-			}
-		}
-	}
-}
+                match (longest_token, longest_match) {
+                    (Some(tok), Some(lexeme)) => {
+                        // move rest past lexeme
+                        rest = &rest[lexeme.len()..];
+                        rest_start_idx += lexeme.len();
 
-/// A stream of tokens
-pub struct TokenStream<'a> {
-	lexer: &'a Lexer<'a>,
-	text_rest: &'a str
-}
+                        // add token to token vector
+                        tokens.push(tok);
+                    }
+                    _ => {
+                        return Err(LexerError::InvalidToken(rest_start_idx));
+                    }
+                }
+            }
 
-impl<'a> TokenStream<'a> {
-	/// Constructs a new stream of tokens, given a lexer and text to lex
-	fn new(lexer: &'a Lexer, text: &'a str) -> TokenStream<'a>  {
-		TokenStream { lexer: lexer, text_rest: text }
-	}
-}
-
-impl<'a> Iterator for TokenStream<'a> {
-	type Item = Result<Token<'a>, LexError<'a>>;
-
-	/// Produces the next token in the stream of tokens generated
-	/// by lexing the given 
-	fn next(&mut self) -> Option<Self::Item> {
-		// get the next token from our internal lexer on its text
-		match self.lexer.get_next_token(self.text_rest) {
-			// end of file
-			None => None,
-			// a token was parsed or error
-			Some(res) => match res {
-				| Ok((tok, rest)) => {
-					self.text_rest = rest;
-					Some(Ok(tok))
-				},
-				| Err(e) => Some(Err(e))
-			}
-		}
-	}
+            Ok(tokens)
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,27 @@ mod lex;
 use crate::lex::{Lexer, Token};
 
 fn main() {
-
     let num_re = Regex::new(r#"\d+"#).unwrap();
     let id_re = Regex::new(r#"[a-zA-Z][a-zA-Z0-9_]+"#).unwrap();
     let ws_re = Regex::new(r#"\s+"#).unwrap();
 
     let mut l = Lexer::new();
 
-    l.add_rule(&num_re, |s| Token::new("number", s));
-    l.add_rule(&id_re, |s| Token::new("name", s));
-    l.add_rule(&ws_re, |s| Token::new("whitespace", s));
+    l.add_pattern(&num_re, |s| Token::new("number", s));
+    l.add_pattern(&id_re, |s| Token::new("name", s));
+    l.add_pattern(&ws_re, |s| Token::new("whitespace", s));
 
-    let tokens = l.lex("hello ^& 10");
+    let lex_result = l.lex("hello ok 10");
 
-    for tok in tokens {
-        match tok {
-            Ok(t) => println!("{:?}", t),
-            Err(e) => { println!("{}", e); break; }
+    match lex_result {
+        Ok(tokens) => {
+            println!("{} tokens.", tokens.len());
+            for tok in tokens.iter() {
+                println!("{:?}", tok);
+            }
+        }
+        Err(e) => {
+            println!("{}", e);
         }
     }
 }
